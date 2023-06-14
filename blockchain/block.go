c0ac69cdb7ce78ad704aa8c2afdd33c2bf26da4a
Creatorjason <107406861+Creatorjason@users.noreply.github.com>
2023-06-14 20:12:13 +0100

    feat: defined data structures for blockchain components

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,1 +1,63 @@
 package blockchain
+
+import (
+	"time"
+)
+
+type (
+	Block struct {
+		BlockHeader  *BlockHeader
+		Transactions []*Transaction
+	}
+
+	Blockchain struct {
+		Blocks []*Block
+	}
+	Transaction struct {
+		ID     []byte
+		TxIn   []TxInputs
+		TxOut  []TxOutputs
+	}
+	BlockHeader struct {
+		MerkleRoot []byte
+		PrevHash   []byte
+		Hash       []byte
+		Timestamp  time.Time
+		Height     int
+	}
+	TxInputs struct {
+		TXID []byte
+		Vout int
+		// sig of sender
+		Sig []byte
+		// pub key/ address of receiver
+		PubKey []byte
+	}
+	TxOutputs struct {
+		Value int
+		Address []byte
+	}
+)
+
+func (bl *Block) DeriveBlockHash() []byte {
+	return nil
+}
+func CreateBlock(trx []*Transaction, prevHash []byte, height int) *Block {
+	return nil
+}
+
+func InitializeChain() *Blockchain {
+	return nil
+}
+
+func CreateTransaction() *Transaction {
+	return nil
+}
+
+func GenesisBlock() *Block {
+	return nil
+}
+
+func (bl *Blockchain) AddBlockHeader() *BlockHeader {
+	return nil
+}
